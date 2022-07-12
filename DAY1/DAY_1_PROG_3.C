08461ed1d8d5ed1f533f6d04a2e5413c36cf6363
SriramaKrishna11 <108974310+SriramaKrishna11@users.noreply.github.com>
2022-07-12 22:46:45 +0530

    Create DAY_1_PROG_3.C

---
+++ DAY1/DAY_1_PROG_3.C
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{
+    float area,radius;
+    printf("enter the radius:\n");
+    scanf("%f",&radius);
+    area=3.14*(radius*radius);
+    printf("area covered = %.3f",area);
+}
