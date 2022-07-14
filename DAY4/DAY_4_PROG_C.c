0ae0cff67294000044c0dc2c9cd5f8895756ef8a
SriramaKrishna11 <108974310+SriramaKrishna11@users.noreply.github.com>
2022-07-14 15:08:37 +0530

    Create DAY_4_PROG_C.c

---
+++ DAY4/DAY_4_PROG_C.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+int i=10,j=20,k,l;
+
+k=i&&j;
+printf("k = %d\n",k);
+
+k=i||j;
+printf("k = %d\n",k);
+
+k=!i;
+printf("k = %d\n",k);
+
+i=10,j=0,k;
+
+k=i&&(j=20);
+printf("i = %d j = %d k = %d\n",i,j,k);
+
+k=i||(j=20);
+printf("i = %d j = %d k = %d\n",i,j,k);
+
+
+i=0,j=0,k=30;
+
+l=i||(j=20)&&k;
+printf("i = %d j = %d k = %d l = %d\n",i,j,k,l);
+
+
+i=10,j=20,k=0;
+
+l=i&&j||j&&k;
+printf("k = %d\n",l);
+
+l=i||j&&i||k;
+printf("k = %d\n",l);
+}
